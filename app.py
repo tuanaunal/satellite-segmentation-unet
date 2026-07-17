39ecfe4bf96995853c37667e26912d9e1445cc61
Tuana <tuanaaunall10@gmail.com>
2026-07-17 17:05:38 +0300

    Initial commit: Proje altyapisi kuruldu ve kutuphaneler iceri aktarildi

---
+++ app.py
@@ -0,0 +1,27 @@
+"""
+problem tanimi: unet kullanarak uydu goruntuleri ile segmentasyon
+
+unet
+uydu goruntuleri: https://www.kaggle.com/datasets/humansintheloop/semantic-segmentation-of-aerial-imagery
+"""
+
+# import libraries
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+
+# veri hazirlama ve yukleme
+
+
+# unet mimarisi tanimlama
+
+
+# egitim asamasi
+
+
+# sonuclarin degerlendirilmesi
